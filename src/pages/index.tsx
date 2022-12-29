29c43001d55d7cc8bde7b7aca66b9505484c2e67
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2022-12-29 09:37:44 -0500

    title updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
-import { type NextPage } from "next";
+import type { NextPage } from "next";
+import Head from "next/head";
 import Experience from "../components/Experience";
 import { Global, css } from "@emotion/react";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Gabe&apos;s Folio :)</title>
+        <meta name="description" content="Gabriel Pedroza's Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Global
         styles={css`
           html,
@@ -13,7 +19,7 @@ const Home: NextPage = () => {
             height: 100%;
             width: 100%;
             overflow: hidden;
-            overflow-y: hidden;
+            overflow-y: hidden;a
             position: fixed;
             top: 0;
             left: 0;
